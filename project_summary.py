259306c8a2e6e7f5e912ebe1cecc998e53f0bc77
Charles M. Wolfe <79966004+CMWolfe922@users.noreply.github.com>
2022-02-06 21:01:49 -0600

    Finished the InputLabel Class

--- project_summary.py
+++ project_summary.py
@@ -5,8 +5,7 @@ class LabelInput(tk.Frame):
 	"""A class that creates a widget containing a label and an input"""
 	
 	def __init__(self, parent, label='', input_class=ttk.Entry,
-                 input_var=None, input_args=None, label_args=None,
-                 **kwargs):
+		input_var=None, input_args=None, label_args=None, **kwargs):
 		"""
 		:param parent: This argument is a reference to the parent widget; 
 			all widgets we create will take this as the first argument
@@ -29,25 +28,53 @@ class LabelInput(tk.Frame):
 		:param **kwargs: Finally, we catch any additional keyword arguments in
 			**kwargs. These will be passed to the Frame constructor.
 		"""
-        super().__init__(parent, **kwargs)
+		super().__init__(parent, **kwargs)
 
-        input_args = input_args or {}
-        label_args = label_args or {}
-        self.variable = input_var
+		input_args = input_args or {}
+		label_args = label_args or {}
+		self.variable = input_var
 
-        if input_class in (ttk.Checkbutton, ttk.Button, ttk.Radiobutton):
-        	input_args["text"] = label
-        	input_args["variable"] = input_var
+		if input_class in (ttk.Checkbutton, ttk.Button, ttk.Radiobutton):
+			input_args["text"] = label
+			input_args["variable"] = input_var
+		else:
+			self.label = ttk.Label(self, text=label, **label_args)
+			self.label.grid(row=0, column=0, sticky=(tk.W + tk.E))
+			input_args["textvariable"] = input_var
+
+		self.input = input_class(self, **input_args)
+		self.input.grid(row=1,column=0, sticky=(tk.W + tk.E))
+		self.columnconfigure(0, weight=1)
+
+
+    def grid(self, sticky=(tk.E + tk.W), **kwargs):
+        super().grid(sticky=sticky, **kwargs)
+
+    def get(self):
+        if self.variable:
+            return self.variable.get()
+        elif type(self.input) == tk.Text:
+            return self.input.get('1.0', tk.END)
         else:
-        	self.label = ttk.Label(self, text=label, **label_args)
-        	self.label.grid(row=0, column=0, sticky=(tk.W + tk.E))
-        	input_args["textvariable"] = input_var
+            return self.input.get()
 
-        self.input = input_class(self, **input_args)
-        self.input.grid(row=1,column=0, sticky=(tk.W + tk.E))
-        self.columnconfigure(0, weight=1)
+    def set(self, value, *args, **kwargs):
+        if type(self.variable) == tk.BooleanVar:
+                self.variable.set(bool(value))
+        elif self.variable:
+                self.variable.set(value, *args, **kwargs)
+        elif type(self.input).__name__.endswith('button'):
+            if value:
+                self.input.select()
+            else:
+                self.input.deselect()
+        elif type(self.input) == tk.Text:
+            self.input.delete('1.0', tk.END)
+            self.input.insert('1.0', value)
+        else:
+            self.input.delete(0, tk.END)
+            self.input.insert(0, value)
 
-        # <<<<<<<<<<<< Pick up at Pg. 52 number 10. >>>>>>>>>>>>>>>>>>>>>>
 
 
 
